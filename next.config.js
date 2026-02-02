c240a1eb09bdec64feb2ad850fd99377fba8ad23
Carlos <robertoambiental@gmail.com>
2026-02-02 23:31:58 +0100

    chore: successful static export setup

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Export static HTML with `next build` when using the App router
+  output: 'export',
+  // If you plan to publish to a project page (username.github.io/repo),
+  // set the assetPrefix or basePath accordingly. Example:
+  // basePath: '/ilovvmelaniss',
+  // assetPrefix: '/ilovvmelaniss',
+}
+
+module.exports = nextConfig
